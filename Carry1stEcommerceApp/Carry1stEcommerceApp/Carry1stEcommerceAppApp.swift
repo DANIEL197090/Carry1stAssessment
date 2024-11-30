3e41f20aa13005e659ace6f3b9ba073bb2398951
DANIEL197090 <ifeanyimbata3@gmail.com>
2024-11-30 21:59:39 +0100

    Created the app project

---
+++ Carry1stEcommerceApp/Carry1stEcommerceApp/Carry1stEcommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carry1stEcommerceAppApp.swift
+//  Carry1stEcommerceApp
+//
+//  Created by Ifeanyi-Mbata on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Carry1stEcommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Carry1stEcommerceApp/Carry1stEcommerceApp/EcommerceTabView.swift
@@ -0,0 +1,18 @@
+//
+//  EcommerceTabView.swift
+//  Carry1stEcommerceApp
+//
+//  Created by Ifeanyi-Mbata on 11/30/24.
+//
+
+import SwiftUI
+
+struct EcommerceTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EcommerceTabView()
+}
